0072e6e327792189aebe26608d1af75a78c40d20
nico <nicolas.chausseau@gmail.com>
2018-02-07 03:58:47 -0500

    FIX: handle a quirk of this codebase: order of calls matters for accessing the data (up to the previous master commit)

--- src/test/java/TestGetPercentageThatReachedFundingGoal.java
+++ src/test/java/TestGetPercentageThatReachedFundingGoal.java
@@ -11,6 +11,8 @@ public class TestGetPercentageThatReachedFundingGoal {
 	public void testNotNull() {
 		
 		CsvReader csvReader = new CsvReader();
+		csvReader.getListOfAllKickstarterRecordsFromCsv(); // ... fix to handle a quirk (the order of calls matters for some part of the history of the project)
+		
 		KickstarterStats classUnderTest = new KickstarterStats(csvReader);
         assertNotNull("making sure that a non-null percentage is returned", classUnderTest.getPercentageThatReachedFundingGoal());
 		
